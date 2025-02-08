8c4a4c17a0b8e0c4431b3c8e9b2a99ca41acb2be
Deen Mohammad <deen9721@gmail.com>
2025-02-08 10:51:08 +0600

    Initial Commit

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bank__Transactions.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext
+            (DbContextOptions<TransactionDbContext> options): base(options)
+        {}
+
+        public DbSet<Transaction>Transactions { get; set; }
+    }
+}
